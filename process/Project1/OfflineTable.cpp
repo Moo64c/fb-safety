b92b48ad8d63b66100e01d431e8acba302f270c2
ishefi <itamarshefi@gmail.com>
2015-04-09 01:28:10 +0300

    evert "changed structs again"
    This reverts commit ea5f3eae46efae8d25f843127918061fc6067896.

--- process/Project1/OfflineTable.cpp
+++ process/Project1/OfflineTable.cpp
@@ -1,7 +1,6 @@
-//offline table
-
 #include "tableStructs.h"
 #include "OfflineTable.h"
+#include "DatabaseConnection.h"
 
 using namespace std;
 
@@ -11,10 +10,26 @@ double freq_threshold = 5;
 
 OfflineTable::OfflineTable(DatabaseConnection &connection) {
 	userData_t data = connection.getNextUserData();
+
 	while (data.userId != 0) {         //userID == 0 : row is NULL
-		if (check(data))
-			printf("found unusual activity, reporting...");
-			updateDB (data);
+		processedEvent_t procceced_data;
+		procceced_data.createdTime = 0;
+		procceced_data.updatedTime = 0;
+		procceced_data.userIdTo = data.userId;
+		procceced_data.frequency = data.frequency;
+		procceced_data.sticky = 1;
+		procceced_data.title = "Weekly Status";
+		if (check(data)) {
+			procceced_data.severity = 5;
+			procceced_data.alert = 1;
+			procceced_data.body = "Unusual activity detected";
+		}
+		else {
+			procceced_data.severity = 0;
+			procceced_data.alert = 0;
+			procceced_data.body = "Everything seems to be OK!";
+		}
+		connection.updateWebInterface(procceced_data);
 		data = connection.getNextUserData();
 	}
 }
